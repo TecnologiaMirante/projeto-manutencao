f4a545a199ae4ad09f7ad6b0bfdff61a7ab86593
José Neto <ribeirojoseph44@gmail.com>
2023-04-14 10:30:28 -0300

    PM-58 :sparkles: feat: equipments-item ui component created

--- web/src/app/equipments/equipments.module.ts
+++ web/src/app/equipments/equipments.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { EquipmentsComponent } from './equipments.component';
 import { EquipmentsRoutingModule } from './equipments-routing.module';
 import { EquipmentsTypeComponent } from './ui/equipments-type/equipments-type.component';
+import { EquipmentsItemComponent } from './ui/equipments-item/equipments-item.component';
 
 @NgModule({
     imports: [
--- web/src/app/equipments/ui/equipments-type/equipments-type.module.ts
+++ web/src/app/equipments/ui/equipments-type/equipments-type.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { EquipmentsTypeComponent } from './equipments-type.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { EquipmentsItemModule } from '../equipments-item/equipments-item.module';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
+        EquipmentsItemModule
     ],
     declarations: [
         EquipmentsTypeComponent
