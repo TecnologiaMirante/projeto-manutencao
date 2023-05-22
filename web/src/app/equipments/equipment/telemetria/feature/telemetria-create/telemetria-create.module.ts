88f9b212c64480ff0dcddb30420334c5e8ff518d
José Neto <ribeirojoseph44@gmail.com>
2023-05-22 15:36:33 -0300

    :recycle: refactor: dropdown

--- web/src/app/equipments/equipment/telemetria/feature/telemetria-create/telemetria-create.module.ts
+++ web/src/app/equipments/equipment/telemetria/feature/telemetria-create/telemetria-create.module.ts
@@ -10,7 +10,7 @@ import { TelemetriaService } from '../../data-access/telemetria.service';
 import { InputTextModule } from 'src/app/shared/ui/input-text/input-text.module';
 import { CancelButtonModule } from 'src/app/shared/ui/cancel-button/cancel-button.module';
 import { VmessageModule } from 'src/app/shared/ui/vmessage/vmessage.module';
-import { InputDropdownModule } from 'src/app/shared/ui/input-dropdown/input-dropdown.module';
+import { InputDropdown2Module } from 'src/app/shared/ui/input-dropdown2/input-dropdown2.module';
 
 
 @NgModule({
@@ -27,7 +27,7 @@ import { InputDropdownModule } from 'src/app/shared/ui/input-dropdown/input-drop
     InputTextModule,
     CancelButtonModule,
     VmessageModule,
-    InputDropdownModule
+    InputDropdown2Module
   ],
   exports: [
     TelemetriaCreateComponent
